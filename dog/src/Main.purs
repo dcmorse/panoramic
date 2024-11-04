25ed0b1849cb6bc4d4c02163a70b47bc53a97b57
Dave Morse <dcmorse@users.noreply.github.com>
2024-11-04 10:05:14 -0600

    route to breed pages, but loading never seems to complete for some reason

--- dog/src/Main.purs
+++ dog/src/Main.purs
@@ -6,15 +6,15 @@ import Affjax.ResponseFormat as AXRF
 import Data.Either (hush)
 import Data.Maybe (Maybe(..), isNothing)
 import Data.Map as Map
-import Data.Array (fromFoldable, (:))
-import Data.Tuple (Tuple(..), uncurry)
+import Data.Array ((:), take)
+import Data.Tuple (Tuple, uncurry)
 import Effect (Effect)
 import Effect.Aff.Class (class MonadAff)
 import Halogen as H
 import Halogen.Aff (awaitBody, runHalogenAff)
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
--- import Halogen.HTML.Properties as HP
+import Halogen.HTML.Properties as HP
 import Halogen.VDom.Driver (runUI)
 -- import Web.Event.Event (Event)
 -- import Web.Event.Event as Event
@@ -39,7 +39,8 @@ mapIndexBreedMap f bmap = map (uncurry f) alist
 
 type State =
   { indexBreedMap :: Maybe IndexBreedMap,
-    breedImages :: Map.Map Breed (Maybe (Array Url)) -- if not in map: never requested. If in map with value Nothing: requested but loading, otherwise loaded
+    breedImages :: Map.Map Breed (Maybe (Array Url)), -- if not in map: never requested. If in map with value Nothing: requested but loading, otherwise loaded
+    route :: Maybe Breed -- 'Nothing' means the index page
   }
 
 data Action
@@ -60,29 +61,44 @@ component =
 
 
 initialState :: forall input. input -> State
-initialState _ = { indexBreedMap: Nothing, breedImages: Map.empty }
+initialState _ = { indexBreedMap: Nothing, breedImages: Map.empty, route: Nothing }
 
 render :: forall m. State -> H.ComponentHTML Action () m
 render st =
-  HH.div_
-    [ HH.p_
-        [ HH.text $ if isNothing st.indexBreedMap then "Loading..." else "" ]
-    , HH.div_
-        case st.indexBreedMap of
-          Nothing -> [ HH.text "no st.result!"]
-          Just bmap ->
-            [ HH.h2_
-                [ HH.text "Response" ]
-            , HH.ul_
-                (map (HH.li_ <<< pure) $ mapIndexBreedMap breedHtml bmap)
-            ]
-    ]
-  where
-    breedHtml breed subbreeds = HH.div_ $ [breedLink breed] <> colon subbreeds <> map (HH.text <<< (\x -> " " <> x)) subbreeds
-    colon [] = []
-    colon _ = [HH.text ":"]
-    breedLink s = HH.a [ HE.onClick \_ -> ViewBreed s, HH.attr (HH.AttrName "href") "#" ] [ HH.text s ]
+  case st.route of
+    Just breed ->
+      HH.div_ $
+        HH.h1_ [ HH.text breed ] : 
+        case Map.lookup breed st.breedImages of
+          Just (Just urls) -> map (\url -> HH.img [ HP.src url ]) (take 20 urls)
+          Just Nothing -> [ HH.text "Loading image list..." ]
+          Nothing -> [ HH.text "Not yet loading image list (this seems like a bug)..." ]
+    Nothing ->
+      HH.div_
+        [ HH.p_
+            [ HH.text $ if isNothing st.indexBreedMap then "Loading..." else "" ]
+        , HH.div_
+            case st.indexBreedMap of
+              Nothing -> [ HH.text "no st.result!"]
+              Just bmap ->
+                [ HH.h2_
+                    [ HH.text "Response" ]
+                , HH.ul_
+                    (map (HH.li_ <<< pure) $ mapIndexBreedMap breedHtml bmap)
+                ]
+        ]
+      where
+        breedHtml breed subbreeds = HH.div_ $ [breedLink breed] <> colon subbreeds <> map (HH.text <<< (\x -> " " <> x)) subbreeds
+        colon [] = []
+        colon _ = [HH.text ":"]
+        breedLink s = HH.a [ HE.onClick \_ -> ViewBreed s, HH.attr (HH.AttrName "href") "#" ] [ HH.text s ]
+   
     
+bodyOf :: forall b rest. { body :: b | rest } -> b
+bodyOf x = x.body
+messageOf :: forall m rest. { message :: m | rest } -> m
+messageOf x = x.message
+
 
 handleAction :: forall output m. MonadAff m => Action -> H.HalogenM State Action () output m Unit
 handleAction action = do
@@ -93,12 +109,22 @@ handleAction action = do
       let
         ensureLoading Nothing = Just Nothing
         ensureLoading x@(Just _) = x
-      H.modify_ \s -> s { breedImages = Map.alter ensureLoading breed s.breedImages }
+      H.modify_ \s -> s { breedImages = Map.alter ensureLoading breed s.breedImages,
+                          route = Just breed
+                        }
       case (Map.lookup breed st.breedImages) of
         Nothing -> do
           H.liftEffect $ log $ "cache miss for " <> breed
           response <- H.liftAff $ AX.get AXRF.string $ "https://dog.ceo/api/breed/" <> breed <> "/images"
           H.liftEffect $ log $ "API response: " <> show (hush response)
+          let parsed :: Maybe { status :: String, message :: Array Url }
+              parsed = join (readJSON_ <$> bodyOf <$> hush response)
+              images :: Maybe (Array Url)
+              images = messageOf <$> parsed
+              updateEntry Nothing = Just images
+              updateEntry mysteriouslyPreCachedImages = mysteriouslyPreCachedImages
+          H.liftEffect $ log $ "updating image cache urls for " <> breed
+          H.modify_ \s -> s { breedImages = Map.alter updateEntry breed s.breedImages }      
         Just _ -> do
           H.liftEffect $ log $ "cache hit for " <> breed
     IndexLoad -> do
@@ -116,9 +142,7 @@ handleAction action = do
       -- Log the response
       H.liftEffect $ log $ "API response: " <> show maybeResponse
 
-      let bodyOf x = x.body
-          messageOf x = x.message
-          maybeBody :: Maybe String
+      let maybeBody :: Maybe String
           maybeBody = bodyOf <$> maybeResponse
           parsed :: Maybe { status :: String, message :: Map.Map Breed (Array Breed) }
           parsed = join (readJSON_ <$> maybeBody)
